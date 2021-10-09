2a6052a70c07fe1c7c02e3d6b9dca1a1e4dec351
mdrafiqulrabin <mdrafiqulrabin@gmail.com>
2021-10-09 07:11:47 -0500

    VariableRenaming (Progress)

--- test_rename_variable.py
+++ test_rename_variable.py
@@ -1,21 +1,23 @@
-import sys
+import config as cf
 from var_renaming import VariableRenaming
 
+
 def main():
-    
-    var_renaming_operator = VariableRenaming(language="java")
+    var_renaming_operator = VariableRenaming(language=cf.JAVA_LANGUAGE)
 
     sample_code = """
         public class Main {
           public static void main(String[] args) {
             int a = 15;
             int b = 20;
-            int c = a + b;
+            int c = a + f(b);
           }
         }
     """
 
-   
+    var_renaming_code = var_renaming_operator.rename_variable(sample_code)
+    print(var_renaming_code)
+
 
 if __name__ == "__main__":
     main()
--- var_renaming.py
+++ var_renaming.py
@@ -1,26 +1,29 @@
-from ast_parser import ASTParser
-import tree_sitter
-from tree_sitter import Node
-from typing import List
+from tree_sitter import Parser
 
-# Not finished yet
-class VariableRenaming()
-    def __init__(self, language: str):
-        super(VariableRenaming, self).__init__(language)
+
+# In Progress
+class VariableRenaming:
+    def __init__(self, language):
+        self.parser = Parser()
+        self.parser.set_language(language)
 
     # Get node with type "identifier"
-    def get_identififer_nodes(self, root, text):
+    def get_identifier_nodes(self, root, text):
         queue = [root]
-        var_declaration_nodes = []
-        var_declarations_types = ['identifier']
+        var_nodes = []
+        var_node_types = {'identifier'}
+        var_parent_types = {'parameter', 'argument', 'variable', 'expression'}
         while queue:
-          current_node = queue.pop(0)
-          for child in current_node.children:
-             child_type = str(child.type)
-             if child_type in var_declarations_types:
-                 var_declaration_nodes.append(child)
-             queue.append(child)
-        return var_declaration_nodes
+            current_node = queue.pop(0)
+            for child in current_node.children:
+                child_type = str(child.type)
+                if child_type in var_node_types:  # only identifier node
+                    parent_type = str(current_node.type)
+                    # filter out class/method name or function call identifier
+                    if any(type_ in parent_type for type_ in var_parent_types):
+                        var_nodes.append(child)
+                queue.append(child)
+        return var_nodes
 
     """
     Group nodes that has the same identifier into the same group
@@ -31,42 +34,27 @@ class VariableRenaming()
         "c": [Nodes]
     }
     """
+
     def group_identifier_nodes(self, identifier_nodes, text):
-        id_groups = {}
+        id_list, id_dct = [], {}
         for identifier_node in identifier_nodes:
             id_name = text[identifier_node.start_byte:identifier_node.end_byte]
-            if id_name not in id_groups:
-                id_groups[id_name] = []
-            id_groups[id_name].append(identifier_node)
-        return id_groups
-
+            id_point = (identifier_node.start_byte, identifier_node.end_byte)
+            id_dct[id_name] = id_dct[id_name] if id_name in id_dct else "var{}".format(len(id_dct) + 1)
+            id_list.append([id_point, id_name, id_dct[id_name]])
+        return id_list
 
     def transform(self, id_groups, text):
-
-        new_t = text
-        for identifier, nodes in id_groups.items():
-            new_identifer = "op"
-
-            print(identifier, "-------------")
-            for n in nodes:
-                print("**********")
-                print(n.start_byte)
-                print(n.start_point)
-                print(n.end_byte)
-                print(n.end_point)
-                # print(text[n.start_byte:n.end_byte])
-                new_t = rename_variable(n, new_t, new_identifer)
-                print(new_t)
-                # text[n.start_byte:n.end_byte] = new_identifer
+        text_list = list(text)
+        id_groups = sorted(id_groups, reverse=True, key=lambda x: x[0])
+        for point, name, rename in id_groups:
+            text_list[:] = text_list[:point[0]] + list(rename) + text_list[point[1]:]
+        return ''.join(text_list)
 
     def rename_variable(self, code_snippet):
-        tree = self.parser.parse(code_snippet)
+        tree = self.parser.parse(bytes(code_snippet, "utf8"))
         root_node = tree.root_node
-        identifier_nodes = get_identififer_nodes(root_node, code_snippet)
-        groups = group_identifier_nodes(identifier_nodes, code_snippet)
-        transform(groups, code_snippet)
-
-        # return new_text
-
-
-
+        identifier_nodes = self.get_identifier_nodes(root_node, code_snippet)
+        identifier_groups = self.group_identifier_nodes(identifier_nodes, code_snippet)
+        renamed_code_snippet = self.transform(identifier_groups, code_snippet)
+        return renamed_code_snippet
